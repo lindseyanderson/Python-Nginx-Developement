2f1dfd7117b3987791d183003565d173a9ed9991
lindseyanderson <lindsey.anderson@rackspace.com>
2013-04-23 01:54:27 -0500

    Update nginx_functions.py
    Updated functions, force include for sites-enabled

--- nginx_functions.py
+++ nginx_functions.py
@@ -8,6 +8,7 @@
 import os, sys
 import commands
 import fnmatch
+import time
 
 def is_nginx_installed():
 	# First release we'll assume php-fpm is installed
@@ -46,21 +47,97 @@ def is_nginx_running():
 def does_virtualhost_exist(site_url=None):
 	# Check virtual host exists
 	base_path = '/etc/nginx'
-	
-	# Transverse the nginx install path, pulling in only
-	# files that will be loaded by nginx
-	for path, subdirectories, files in os.walk(base_path):
-		for name in fnmatch.filter(files, '*.conf'):
-			current_file = os.path.join(path,name)
-			with open(current_file) as file_data:
-				for line in file_data:
-					if not line.startswith('#'):
-						if 'server_name' in line:
-							found_alias =  line.split()[1]
-							found_alias = found_alias[:-1]
-							if found_alias == site_url:
-								return True
+
+	# Get a list of all files within the base path that end in .conf
+	files = [ os.path.join(subdir, filename)
+			for (subdir, directories, files) in os.walk(base_path)
+			for filename in fnmatch.filter(files, '*.conf') ]
+
+	# Pull all server_name directives that are active within those files
+	server_blocks = [ line for file in files
+			for line in open(file,'r')
+			if 'server_name' in line
+			if not line.startswith('#') ]
+
+	# We need to find only the actual data, strip the fat away
+	# multiple server_names will result in sublists
+	stripped_blocks = [ found_urls.strip(' server_name \;\n') for found_urls in server_blocks ]
+
+	# flatten the lists
+	nested_blocks = [ block.split(' ') for block in stripped_blocks ]
+
+	server_names = [ value for sublist in nested_blocks for value in sublist ]
+
+	# See if our site_url is in the list
+	for server_name in server_names:
+		if server_name == site_url:
+			print "Site URL exists as an active server_name"
+			return True
 	return False
 
-print does_virtualhost_exist(site_url="${site_url}")
 
+def check_directories_exist():
+	available_directory = '/etc/nginx/sites-available'
+	enabled_directory   = '/etc/nginx/sites-enabled'
+
+	if not os.path.exists(available_directory):
+		try:
+			os.makedirs(available_directory)
+			return True
+		except:
+			print "Something bad happened!"
+			sys.exit(0)
+	elif not os.path.exists(enabled_directory):
+		try:
+			os.makedirs(enabled_directory)
+			return True
+		except:
+			print "Something bad happened!"
+			sys.exit(0)
+	else:
+		return True
+	return False
+
+
+def check_nginxconf_includes():
+	# Testing purposes
+	input_file = 'nginx.conf'
+	output_file = 'nginx_new.conf'
+	append_line = '    include /etc/nginx/sites-enabled/*;\n'
+
+	# Check for our include line in the nginx configuration
+	with open(input_file, 'r') as file_data:
+		content = file_data.readlines()
+		if append_line in content:
+			return True
+
+	# Find the line we can safely insert our include into
+	line_number = [ i+1 for i, line in enumerate(open(input_file, 'r'))
+                if 'http {' in line ]
+
+	# Get data from old nginx.conf, this will be our backup
+	old_file = open(input_file, "r")
+	new_file_array = []
+	for line_num, line in enumerate(old_file):
+		try:
+	        	if line_num == line_number[0]:
+	        	        new_file_array.append(append_line)
+			new_file_array.append(line)
+		except:
+			new_file_array.append(line)
+
+	# Create new nginx.conf
+	new_file = open(output_file, "w")
+	for item in new_file_array:
+	        new_file.write("%s" % item)
+
+	# Move our old nginx configuration to a backup
+	temp_file_path = input_file
+	os.rename(input_file, input_file + '-OLD')
+	os.rename(output_file, temp_file_path)
+
+	# Our include now exists
+	return True
+
+
+print check_nginxconf_includes()
